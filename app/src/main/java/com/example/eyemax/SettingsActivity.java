6f0c654dfa2299d1c4d533659ba6e19731dea9c3
Benji Spetter-Goldstein <benjisg@bu.edu>
2020-11-25 20:16:15 -0500

    add clear cache and restore defaults preferences

--- app/src/main/java/com/example/eyemax/SettingsActivity.java
+++ app/src/main/java/com/example/eyemax/SettingsActivity.java
@@ -1,22 +1,27 @@
 package com.example.eyemax;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SeekBarPreference;
 
 /*
  * Activity for updating preferences
  */
 
 public class SettingsActivity extends AppCompatActivity implements SharedPreferences
-        .OnSharedPreferenceChangeListener
-{
+        .OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,68 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.settings_pref, rootKey);
+
+            Preference clearCacheButton = findPreference("resetCacheButton");
+            clearCacheButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    //code for what you want it to do
+
+                    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which){
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    //Yes button clicked
+                                    SharedPreferences settings = getContext().getSharedPreferences(
+                                            "PreferencesName", Context.MODE_PRIVATE);
+                                    settings.edit().clear().commit();
+                                    break;
+
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    //No button clicked
+                                    break;
+                            }
+                        }
+                    };
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setMessage("WARNING: This will remove all cached data. New data will need to be fetched resulting in additional API requests and slower page loading. Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                            .setNegativeButton("No", dialogClickListener).show();
+
+                    return true;
+                }
+            });
+
+            SeekBarPreference matchConfidenceSeekBarPref = findPreference(getString(R.string.match_sensitivity));
+
+            Preference restoreDefaultsButton = findPreference("restoreDefaultsButton");
+            restoreDefaultsButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which){
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    //Yes button clicked
+                                    matchConfidenceSeekBarPref.setValue(70);
+                                    break;
+
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    //No button clicked
+                                    break;
+                            }
+                        }
+                    };
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setMessage("This will reset all preferences to their default values. Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                            .setNegativeButton("No", dialogClickListener).show();
+
+                    return true;
+                }
+            });
         }
     }
 
