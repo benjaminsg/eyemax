5dd9d3d7edc13da8568a5a8452b21fc1c7906e18
Benji Spetter-Goldstein <benjisg@bu.edu>
2020-11-25 22:10:17 -0500

    remove unused import

--- app/src/main/java/com/example/eyemax/MainActivity.java
+++ app/src/main/java/com/example/eyemax/MainActivity.java
@@ -24,7 +24,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Toast;
